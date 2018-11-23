2ce37ce440b1144c4da36a8caaefefb1b9792b57
hanirr3 <44904038+hanirr3@users.noreply.github.com>
2018-11-23 19:35:06 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,29 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`**
+    عندنا اقوى نظام انفايت 
+    5 = 15k credit pro bot
+  بنسوي فعاليه على 100 الف كريدت
+
+                                 [ https://discord.gg/bf59j2] **`)
+}).catch(console.error)
+})
+
+client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`**
+     عندنا اقوى نظام انفايت 
+    5 = 15k credit pro bot
+  بنسوي فعاليه على 100 الف كريدت
+
+                                 [ https://discord.gg/bf59j2] **`)
+}).catch(console.error)
+})
+client.login(process.env.BOT_TOKEN);
